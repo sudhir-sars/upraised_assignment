7070ab05cf9f64b22c0776a20c2e7c99cf1bbe95
sudhir-sars <sudhir.sars@gmail.com>
2025-02-08 19:12:52 +0530

    feat: set up Express server

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// src/index.ts
+import app from './app';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
